57c3b2ea401ec8f82155debec670f2794b68ed34
Mark IJbema <mark@kilocode.ai>
2025-06-26 19:39:50 +0200

    also fix these translations ni script

--- scripts/kilocode/roomerge_80_fix_roo_in_translations.sh
+++ scripts/kilocode/roomerge_80_fix_roo_in_translations.sh
@@ -5,6 +5,7 @@
 
 # Fix i18n locale JSON files
 find src/i18n/locales -name "*.json" -type f -exec sed -i '' -E 's/(^|[^a-zA-Z])Roo Code([^a-zA-Z]|$)/\1Kilo Code\2/g' {} \;
+find webview-ui/src/i18n/locales/ -name "*.json" -type f -exec sed -i '' -E 's/(^|[^a-zA-Z])Roo Code([^a-zA-Z]|$)/\1Kilo Code\2/g' {} \;
 
 # Fix package.nls JSON files
 find . -name "package.nls*.json" -type f -exec sed -i '' -E 's/(^|[^a-zA-Z])Roo Code([^a-zA-Z]|$)/\1Kilo Code\2/g' {} \;
