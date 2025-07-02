96e3fe71fbf4a9c5b7eee776693421a52728750c
Eamon Nerbonne <eamon@nerbonne.org>
2025-07-02 21:16:06 +0200

    Support staging too

--- src/utils/kilocode-token.ts
+++ src/utils/kilocode-token.ts
@@ -4,6 +4,7 @@ export function getKiloBaseUriFromToken(kilocodeToken: string) {
 		const payload = JSON.parse(Buffer.from(payload_string, "base64").toString())
 		//note: this is UNTRUSTED, so we need to make sure we're OK with this being manipulated by an attacker; e.g. we should not read uri's from the JWT directly.
 		if (payload.env === "development") return "http://localhost:3000"
+		if (payload.env === "staging" || payload.env === "preview") return "https://staging.kilocode.ai"
 	} catch (_error) {
 		console.warn("Failed to get base URL from Kilo Code token")
 	}
