412dc46ee0d1e154355751f8f5122d5408716c19
oliver-14203 <oburton.au@gmail.com>
2025-10-22 19:39:32 +0800

    Add 12 new themes from Google Gemini CLI

--- cli/src/constants/themes/index.ts
+++ cli/src/constants/themes/index.ts
@@ -11,6 +11,17 @@ import type { Theme, ThemeId } from "../../types/theme.js"
 import { alphaTheme } from "./alpha.js"
 import { darkTheme } from "./dark.js"
 import { lightTheme } from "./light.js"
+import { draculaTheme } from "./dracula.js"
+import { atomOneDarkTheme } from "./atom-one-dark.js"
+import { ayuDarkTheme } from "./ayu-dark.js"
+import { githubDarkTheme } from "./github-dark.js"
+import { githubLightTheme } from "./github-light.js"
+import { googleCodeTheme } from "./googlecode.js"
+import { xcodeTheme } from "./xcode.js"
+import { shadesOfPurpleTheme } from "./shades-of-purple.js"
+import { ayuLightTheme } from "./ayu-light.js"
+import { ansiTheme } from "./ansi.js"
+import { ansiLightTheme } from "./ansi-light.js"
 
 /**
  * Registry of all available themes
@@ -19,6 +30,17 @@ const themeRegistry: Record<ThemeId, Theme> = {
 	dark: darkTheme,
 	light: lightTheme,
 	alpha: alphaTheme,
+	dracula: draculaTheme,
+	"atom-one-dark": atomOneDarkTheme,
+	"ayu-dark": ayuDarkTheme,
+	"github-dark": githubDarkTheme,
+	"github-light": githubLightTheme,
+	googlecode: googleCodeTheme,
+	xcode: xcodeTheme,
+	"shades-of-purple": shadesOfPurpleTheme,
+	"ayu-light": ayuLightTheme,
+	ansi: ansiTheme,
+	"ansi-light": ansiLightTheme,
 }
 
 /**
@@ -52,3 +74,14 @@ export type { Theme, ThemeId } from "../../types/theme.js"
 export { darkTheme } from "./dark.js"
 export { lightTheme } from "./light.js"
 export { alphaTheme } from "./alpha.js"
+export { draculaTheme } from "./dracula.js"
+export { atomOneDarkTheme } from "./atom-one-dark.js"
+export { ayuDarkTheme } from "./ayu-dark.js"
+export { githubDarkTheme } from "./github-dark.js"
+export { githubLightTheme } from "./github-light.js"
+export { googleCodeTheme } from "./googlecode.js"
+export { xcodeTheme } from "./xcode.js"
+export { shadesOfPurpleTheme } from "./shades-of-purple.js"
+export { ayuLightTheme } from "./ayu-light.js"
+export { ansiTheme } from "./ansi.js"
+export { ansiLightTheme } from "./ansi-light.js"
