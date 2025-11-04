c92c9c59a9948a55f7843cfcf84802f5da0b5129
Catriel Müller <catrielmuller@gmail.com>
2025-11-04 15:34:09 +0100

    refactor: fix merge conflict

--- src/services/continuedev/core/autocomplete/CompletionProvider.ts
+++ src/services/continuedev/core/autocomplete/CompletionProvider.ts
@@ -55,7 +55,6 @@ export class CompletionProvider {
 			return undefined
 		}
 
-
 		// Temporary fix for JetBrains autocomplete bug as described in https://github.com/continuedev/continue/pull/3022
 		if (llm.model === undefined && llm.completionOptions?.model !== undefined) {
 			llm.model = llm.completionOptions.model
@@ -212,18 +211,9 @@ export class CompletionProvider {
 
 				// Don't postprocess if aborted
 				if (token.aborted) {
-<<<<<<< HEAD
 					return undefined
 				}
 
-=======
-					console.log('aborted')
-					return undefined
-				}
-
-				console.log('raw completion', completion)
-
->>>>>>> 40ab73e3d8 (Reset formatting changes to upstream versions)
 				const processedCompletion = helper.options.transform
 					? postprocessCompletion({
 							completion,
