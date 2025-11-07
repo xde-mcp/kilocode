db1b2fa151eb9e8ecc096bf8df14bceabcd91b4a
beatlevic <beatlevic@gmail.com>
2025-11-07 21:00:07 +0100

    Fixed AutocompleteLruCacheInMem.test.ts import

--- src/services/continuedev/core/autocomplete/util/AutocompleteLruCacheInMem.test.ts
+++ src/services/continuedev/core/autocomplete/util/AutocompleteLruCacheInMem.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, beforeEach } from "vitest"
-import { AutocompleteLruCacheInMem } from "./AutoCompleteLruCacheInMem"
+import { AutocompleteLruCacheInMem } from "./AutocompleteLruCacheInMem"
 
 describe("AutoCompleteLruCacheInMem", () => {
 	let cache: AutocompleteLruCacheInMem
