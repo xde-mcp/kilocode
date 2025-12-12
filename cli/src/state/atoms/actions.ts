cdf6357d990debac4101b98d57ead21df7f03d97
Eng. Juan Combetto <omniwired@gmail.com>
2025-12-13 07:48:54 +0900

    refactor: move yoloModeAtom import to top of file

--- cli/src/state/atoms/actions.ts
+++ cli/src/state/atoms/actions.ts
@@ -6,7 +6,7 @@
 import { atom } from "jotai"
 import type { WebviewMessage, ProviderSettings, ClineAskResponse } from "../../types/messages.js"
 import { extensionServiceAtom, isServiceReadyAtom, setServiceErrorAtom } from "./service.js"
-import { resetMessageCutoffAtom } from "./ui.js"
+import { resetMessageCutoffAtom, yoloModeAtom } from "./ui.js"
 import { logs } from "../../services/logs.js"
 
 /**
@@ -305,8 +305,6 @@ export const sendSecondaryButtonClickAtom = atom(null, async (get, set) => {
  * Sends the yoloMode message to the extension to enable/disable auto-approval of all operations
  */
 export const toggleYoloModeAtom = atom(null, async (get, set) => {
-	const { yoloModeAtom } = await import("./ui.js")
-
 	const currentValue = get(yoloModeAtom)
 	const newValue = !currentValue
 
