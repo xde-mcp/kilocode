0c090143d0afc5180ce1e3b1b6875d15c537ab5f
Mark IJbema <mark@kilocode.ai>
2026-01-08 16:00:23 +0100

    Remove fileNotFound logic from task history
    - Remove fileNotfound field from HistoryItem schema
    - Remove setTaskFileNotFound method from ClineProvider
    - Restore original deleteTaskFromState behavior when task file is not found
    
    This completes the revert of PR #938's fileNotFound feature which was
    marking tasks with a red background when their files couldn't be found.

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -1816,8 +1816,7 @@ export class ClineProvider
 
 		// if we tried to get a task that doesn't exist, remove it from state
 		// FIXME: this seems to happen sometimes when the json file doesnt save to disk for some reason
-		// await this.deleteTaskFromState(id) // kilocode_change disable confusing behaviour
-		await this.setTaskFileNotFound(id) // kilocode_change
+		await this.deleteTaskFromState(id)
 		throw new Error("Task not found")
 	}
 
@@ -3665,19 +3664,6 @@ Here is the project's README to help you get started:\n\n${mcpDetails.readmeCont
 		for (const id of taskIds) {
 			await this.deleteTaskWithId(id)
 		}
-	}
-
-	async setTaskFileNotFound(id: string) {
-		const history = this.getGlobalState("taskHistory") ?? []
-		const updatedHistory = history.map((item) => {
-			if (item.id === id) {
-				return { ...item, fileNotfound: true }
-			}
-			return item
-		})
-		await this.updateGlobalState("taskHistory", updatedHistory)
-		this.kiloCodeTaskHistoryVersion++
-		await this.postStateToWebview()
 	}
 
 	private kiloCodeTaskHistoryVersion = 0
