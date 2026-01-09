237fa89b7e064c3ecbf6675af69e8ccb0380a34c
Chris Estreich <cestreich@gmail.com>
2026-01-09 13:12:22 -0800

    Add some slash commands that are useful for cli development (#10586)

--- packages/core/src/debug-log/index.ts
+++ packages/core/src/debug-log/index.ts
@@ -5,7 +5,7 @@
  * which would break TUI applications. The log format is timestamped JSON.
  *
  * Usage:
- *   import { debugLog, DebugLogger } from "@roo-code/core/debug-log"
+ *   import { debugLog, DebugLogger } from "@roo-code/core/cli"
  *
  *   // Simple logging
  *   debugLog("handleModeSwitch", { mode: newMode, configId })
