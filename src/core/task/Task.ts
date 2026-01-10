e39abbffa17316543f1d19cbb0c3050170ed4837
Hannes Rudolph <hrudolph@gmail.com>
2026-01-09 18:32:13 -0700

    fix: make edit_file matching more resilient (#10585)

--- src/core/task/Task.ts
+++ src/core/task/Task.ts
@@ -354,6 +354,7 @@ export class Task extends EventEmitter<TaskEvents> implements TaskLike {
 	consecutiveMistakeCount: number = 0
 	consecutiveMistakeLimit: number
 	consecutiveMistakeCountForApplyDiff: Map<string, number> = new Map()
+	consecutiveMistakeCountForEditFile: Map<string, number> = new Map()
 	consecutiveNoToolUseCount: number = 0
 	consecutiveNoAssistantMessagesCount: number = 0
 	toolUsage: ToolUsage = {}
