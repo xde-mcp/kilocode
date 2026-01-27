7ae149b3d358e3aca82509e130e0f6595b0c2339
Brendan O'Leary <brendan@olearycrew.com>
2026-01-27 15:56:58 +0100

    Remove basepath

--- apps/kilocode-docs/next.config.js
+++ apps/kilocode-docs/next.config.js
@@ -3,7 +3,6 @@ const previousDocsRedirects = require("./previous-docs-redirects")
 
 module.exports = withMarkdoc(/* config: https://markdoc.io/docs/nextjs#options */)({
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdoc"],
-	basePath: "/docs",
 	turbopack: {},
 	experimental: {
 		instrumentationHook: true,
